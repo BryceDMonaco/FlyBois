42ada1ee72826d9ae7b85bc415b376708a15dafa
Bryce Monaco <Bryce@BrycBook-Pro.local>
2018-10-02 10:01:55 -0700

    Goals emit confetti when flown through

--- Assets/_Scripts/Goal.cs
+++ Assets/_Scripts/Goal.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour {
 
+	public GameObject confetti;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +14,17 @@ public class Goal : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.name == "PlaneObject") //Checking for a tag would be faster
+		{
+			GameObject con = Instantiate (confetti, transform.position, transform.rotation);
+
+			Destroy (con, 10f);
+
+		}
+
 	}
 }
